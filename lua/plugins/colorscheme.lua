bd763fede1bd876505c356d30a22f02c6ccd9df8
Sagar Hathwar <sagarbhathwar@gmail.com>
2024-09-11 05:40:15 -0700

    (minor) Commands to toggle auto-formatting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,59 +1,9 @@
 return {
+
   {
-    {
-      "doums/darcula",
-      lazy = true,
-    },
-    {
-      "folke/tokyonight.nvim",
-      lazy = true,
-      opts = { style = "moon" },
-    },
-
-    {
-      "overcache/NeoSolarized",
-      lazzy = true,
-    },
-
-    "catppuccin/nvim",
+    "folke/tokyonight.nvim",
     lazy = true,
-    name = "catppuccin",
-    opts = {
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
+    opts = { style = "moon" },
   },
+
 }
